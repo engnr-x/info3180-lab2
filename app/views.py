639e4d2dae6a0b8fe8e00f41a60394bff60291b1
Ashani Mae <flava4lyphe92@gmail.com>
2025-02-23 20:02:13 -0500

    initial commit of working app

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 
 ###
@@ -16,6 +17,16 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    date_joined = datetime(2025, 2, 21)
+    format_date = format_date_joined(date_joined)
+
+    return render_template('profile.html', name="Ashani Mae", date_joined=format_date)
+
+def format_date_joined(date):
+    return date.strftime('%b, %Y')
 
 
 ###
